71196e58f4611f87ab6337d9ef015453f661440b
Vikash Singh <vikashlikes18@gmail.com>
2021-07-27 20:53:59 +0530

    Update
    kite.py updated

--- kite.py
+++ kite.py
@@ -6,28 +6,29 @@ from selenium.webdriver.support import expected_conditions as EC
 #read credentials from file
 f = open("cred.txt", "r")
 lines = f.readlines()
-usernameStr = lines[0]
-passwordStr = lines[1]
-pinStr = lines[2]
+usernameStr = lines[0] #username
+passwordStr = lines[1] #password
+pinStr = lines[2]      #pin
 f.close()
 
 
 browser = webdriver.Firefox()
 browser.get(('https://kite.zerodha.com/'))
 signInButton= browser.find_element_by_css_selector('button.button-orange.wide')
+
 # fill in username and hit the next button
 username = browser.find_element_by_id('userid')
 username.send_keys(usernameStr)
 
+#click next input field
 nextButton = browser.find_element_by_id('password')
-# nextButton = browser.find_element_by_xpath("//form[@id='login-form']/input[2]")
 nextButton.click()
 
 # wait for transition then continue to fill items
-
 password = WebDriverWait(browser, 2).until(EC.presence_of_element_located((By.ID, "password")))
 password.send_keys(passwordStr)
 
+#click button to continue
 signInButton.click()
 
 # wait for transition then continue to fill items
@@ -35,4 +36,5 @@ signInButton.click()
 pin = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "pin")))
 pin.send_keys(pinStr)
 
+#click button to continue
 signInButton.click()
