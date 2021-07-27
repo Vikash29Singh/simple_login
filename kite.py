a231b05d61b80b5af930ec5a54b5ee441225fbf3
Vikash Singh <vikashlikes18@gmail.com>
2021-07-27 20:46:05 +0530

    New
    New file added

---
+++ kite.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# usernameStr = 'NA5676'
+# passwordStr = 'Vis@1829VS'
+# pinStr = '192996'
+
+#read credentials from file
+f = open("cred.txt", "r")
+lines = f.readlines()
+usernameStr = lines[0]
+passwordStr = lines[1]
+pinStr = lines[2]
+f.close()
+
+
+browser = webdriver.Firefox()
+browser.get(('https://kite.zerodha.com/'))
+signInButton= browser.find_element_by_css_selector('button.button-orange.wide')
+# fill in username and hit the next button
+username = browser.find_element_by_id('userid')
+username.send_keys(usernameStr)
+
+nextButton = browser.find_element_by_id('password')
+# nextButton = browser.find_element_by_xpath("//form[@id='login-form']/input[2]")
+nextButton.click()
+
+# wait for transition then continue to fill items
+
+password = WebDriverWait(browser, 2).until(EC.presence_of_element_located((By.ID, "password")))
+password.send_keys(passwordStr)
+
+signInButton.click()
+
+# wait for transition then continue to fill items
+
+pin = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "pin")))
+pin.send_keys(pinStr)
+
+signInButton.click()
