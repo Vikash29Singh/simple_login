b16bdce0fb8366b019a6e49676afc543ac52b188
Vikash Singh <vikashlikes18@gmail.com>
2021-08-17 10:43:43 +0530

    readme.md and few changes

--- README.md
+++ README.md
@@ -41,15 +41,22 @@ browser.get(('https://www.google.com/'))
 For more detail [click me.](https://selenium-python.readthedocs.io/locating-elements.html)
 
 # Make python file executable
+
 The first step is to install PyInstaller from PyPI. 
 ~~~
 pip install pyinstaller
 ~~~
 Once installed successfully go to the directory you have installed the .py file
 ~~~
-pyinstaller Yourfilename.py
+pyinstaller --onefile Yourfilename.py
 ~~~
 Your executable file is successfully created.
+
+### This command will create additional files and folders. Once creation is completed, navigate to 'dist' folder and "Voila!" you will see executable file. Run the file.
+<br>
+
+### <span style="color:red">Note:- </span>To execute my code, first make changes in the "cred.txt" file and save the file in dist folder, where executable is generated. <span style="color:red">Or</span> instead of creating the separate credential file, simply add the credential in your main python file and then run the pyinstaller instruction as above. Now, all you need to do is run the executable file once created successfully.
+<br>
 
 To know more visit [pypi](https://pypi.org/project/pyinstaller/) or [realpython](https://realpython.com/pyinstaller-python/#using-pyinstaller)
 
--- kite.py
+++ kite.py
@@ -1,45 +1,45 @@
-#This is just a demo app to login into zerodha.
+# This is just a demo app to login into zerodha.
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#read credentials from file
-#its always safe to store credential in different file. 
-#Saving credential in the script is not a good practise.
+# read credentials from file
 f = open("cred.txt", "r")
 lines = f.readlines()
-#pass parameter based on your requirement
-usernameStr = lines[0] #username
-passwordStr = lines[1] #password
-pinStr = lines[2]      #pin
+# pass parameter based on your requirement
+usernameStr = lines[0]  # username
+passwordStr = lines[1]  # password
+pinStr = lines[2]  # pin
 f.close()
-
 
 browser = webdriver.Firefox()
 browser.get(('https://kite.zerodha.com/'))
-signInButton= browser.find_element_by_css_selector('button.button-orange.wide')
+signInButton = browser.find_element_by_css_selector(
+    'button.button-orange.wide')
 
 # fill in username and hit the next button
 username = browser.find_element_by_id('userid')
 username.send_keys(usernameStr)
 
-#click next input field
+# click next input field
 nextButton = browser.find_element_by_id('password')
 nextButton.click()
 
 # wait for transition then continue to fill items
-password = WebDriverWait(browser, 2).until(EC.presence_of_element_located((By.ID, "password")))
+password = WebDriverWait(browser, 2).until(
+    EC.presence_of_element_located((By.ID, "password")))
 password.send_keys(passwordStr)
 
-#click button to continue
+# click button to continue
 signInButton.click()
 
 # wait for transition then continue to fill items
 
-pin = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "pin")))
+pin = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.ID, "pin")))
 pin.send_keys(pinStr)
 
-#click button to continue
+# click button to continue
 signInButton.click()
